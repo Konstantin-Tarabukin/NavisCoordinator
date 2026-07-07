4ce888c4ba9e565552ed6566e069269045d56c6f
Konstantin Tarabuikin <ktarabukin@gmail.com>
2026-07-07 11:20:25 +0300

    Добавьте файлы проекта.

---
+++ NavisCoordinator/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Navisworks.Api.Plugins;
+using System.Windows.Forms;
+
+namespace NavisCoordinator
+{
+    [Plugin(
+        "NavisCoordinator",
+        "CT",
+        DisplayName = "Navis Coordinator")]
+    public class Plugin : AddInPlugin
+    {
+        public override int Execute(params string[] parameters)
+        {
+            MessageBox.Show(
+                "Плагин успешно загружен!",
+                "Navis Coordinator");
+
+            return 0;
+        }
+    }
+}
